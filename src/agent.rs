d95f0935756e669e97a6bcb1c2c14e89a46e8377
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-24 12:53:58 +0900

    add agent#patch

--- src/agent.rs
+++ src/agent.rs
@@ -35,6 +35,14 @@ impl Agent {
     {
         self.client.put(path.into()).body(payload.into()).await
     }
+
+    pub async fn patch<S, T>(&self, path: S, payload: T) -> Result<surf::Response, surf::Error>
+    where
+        S: Into<String>,
+        T: Into<surf::Body>,
+    {
+        self.client.patch(path.into()).body(payload.into()).await
+    }
 }
 
 #[cfg(test)]
@@ -107,6 +115,30 @@ mod tests {
 
         let agent = Agent::new(base_url);
         let mut response = agent.put(path, payload).await?;
+        assert_eq!(response.status(), surf::StatusCode::Created);
+        assert_eq!(response.header(header_name).unwrap(), header_value);
+        assert_eq!(response.body_string().await?, body);
+
+        Ok(())
+    }
+
+    #[async_std::test]
+    async fn test_agent_patch() -> surf::Result<()> {
+        let base_url = &mockito::server_url();
+        let path = "/hello";
+        let payload = json!({ "hello": "world"});
+        let header_name = "content-type";
+        let header_value = "text/plain";
+        let body = r#"{"hello": "world"}"#;
+
+        let _m = mockito::mock("PATCH", path)
+            .with_status(surf::StatusCode::Created as usize)
+            .with_header(header_name, header_value)
+            .with_body(body)
+            .create();
+
+        let agent = Agent::new(base_url);
+        let mut response = agent.patch(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Created);
         assert_eq!(response.header(header_name).unwrap(), header_value);
         assert_eq!(response.body_string().await?, body);
