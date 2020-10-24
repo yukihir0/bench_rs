1e93782ce73b826a95ebd4675c38fa68cfecd9e7
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-24 10:51:36 +0900

    add agent#get

--- src/agent.rs
+++ src/agent.rs
@@ -1,7 +1,51 @@
-pub struct Agent {}
+use surf;
+use url::Url;
+
+pub struct Agent {
+    client: surf::Client,
+}
 
 impl Agent {
-    pub fn new() -> Agent {
-        Agent {}
+    pub fn new(base_url: &str) -> Agent {
+        let mut client = surf::client();
+        client.set_base_url(Url::parse(base_url).unwrap());
+
+        Agent { client: client }
+    }
+
+    pub async fn get<S>(&self, path: S) -> Result<surf::Response, surf::Error>
+    where
+        S: Into<String>,
+    {
+        self.client.get(path.into()).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::agent::*;
+    use mockito;
+
+    #[async_std::test]
+    async fn test_agent_get() -> surf::Result<()> {
+        let base_url = &mockito::server_url();
+        let path = "/hello";
+        let header_name = "content-type";
+        let header_value = "text/plain";
+        let body = "hello, world!!";
+
+        let _m = mockito::mock("GET", path)
+            .with_status(surf::StatusCode::Ok as usize)
+            .with_header(header_name, header_value)
+            .with_body(body)
+            .create();
+
+        let agent = Agent::new(base_url);
+        let mut response = agent.get(path).await?;
+        assert_eq!(response.status(), surf::StatusCode::Ok);
+        assert_eq!(response.header(header_name).unwrap(), header_value);
+        assert_eq!(response.body_string().await?, body);
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate bench_rs;
 
-use bench_rs::agent::*;
+use async_std;
 use bench_rs::benchmark::*;
 use bench_rs::score::*;
 
-fn main() {
-    let agent = Agent::new();
+#[async_std::main]
+async fn main() -> surf::Result<()> {
     let score = Score::new();
     let benchmark = Benchmark::new(score);
     let benchmark_result = benchmark.start();
 
     println!("total: {}", benchmark_result.total_score());
+
+    Ok(())
 }
