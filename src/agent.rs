383dde78b373840bbf2621f615d4ba2f394d7166
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-24 12:58:46 +0900

    add agent#delete

--- src/agent.rs
+++ src/agent.rs
@@ -43,6 +43,14 @@ impl Agent {
     {
         self.client.patch(path.into()).body(payload.into()).await
     }
+
+    pub async fn delete<S, T>(&self, path: S, payload: T) -> Result<surf::Response, surf::Error>
+    where
+        S: Into<String>,
+        T: Into<surf::Body>,
+    {
+        self.client.delete(path.into()).body(payload.into()).await
+    }
 }
 
 #[cfg(test)]
@@ -140,6 +148,30 @@ mod tests {
         let agent = Agent::new(base_url);
         let mut response = agent.patch(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Created);
+        assert_eq!(response.header(header_name).unwrap(), header_value);
+        assert_eq!(response.body_string().await?, body);
+
+        Ok(())
+    }
+
+    #[async_std::test]
+    async fn test_agent_delete() -> surf::Result<()> {
+        let base_url = &mockito::server_url();
+        let path = "/hello";
+        let payload = json!({ "hello": "world"});
+        let header_name = "content-type";
+        let header_value = "text/plain";
+        let body = r#"{"hello": "world"}"#;
+
+        let _m = mockito::mock("DELETE", path)
+            .with_status(surf::StatusCode::Ok as usize)
+            .with_header(header_name, header_value)
+            .with_body(body)
+            .create();
+
+        let agent = Agent::new(base_url);
+        let mut response = agent.delete(path, payload).await?;
+        assert_eq!(response.status(), surf::StatusCode::Ok);
         assert_eq!(response.header(header_name).unwrap(), header_value);
         assert_eq!(response.body_string().await?, body);
 
