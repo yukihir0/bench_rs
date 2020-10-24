9dd307683957b436f537c6bdb22418fdb3da77ff
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-24 19:23:57 +0900

    add agent#set_user_agent

--- src/agent.rs
+++ src/agent.rs
@@ -1,8 +1,10 @@
 use surf;
 use url::Url;
 
+#[derive(Clone)]
 pub struct Agent {
     client: surf::Client,
+    user_agent: String,
 }
 
 impl Agent {
@@ -10,11 +12,21 @@ impl Agent {
         let mut client = surf::client();
         client.set_base_url(Url::parse(&base_url.into()).unwrap());
 
-        Agent { client: client }
+        Agent {
+            client: client,
+            user_agent: String::from(""),
+        }
+    }
+
+    pub fn set_user_agent(&mut self, user_agent: impl Into<String>) {
+        self.user_agent = user_agent.into();
     }
 
     pub async fn get(&self, path: impl Into<String>) -> Result<surf::Response, surf::Error> {
-        self.client.get(path.into()).await
+        self.client
+            .get(path.into())
+            .header("User-Agent", self.user_agent.clone())
+            .await
     }
 
     pub async fn post(
@@ -22,7 +34,11 @@ impl Agent {
         path: impl Into<String>,
         payload: impl Into<surf::Body>,
     ) -> Result<surf::Response, surf::Error> {
-        self.client.post(path.into()).body(payload.into()).await
+        self.client
+            .post(path.into())
+            .header("User-Agent", self.user_agent.clone())
+            .body(payload.into())
+            .await
     }
 
     pub async fn put(
@@ -30,7 +46,11 @@ impl Agent {
         path: impl Into<String>,
         payload: impl Into<surf::Body>,
     ) -> Result<surf::Response, surf::Error> {
-        self.client.put(path.into()).body(payload.into()).await
+        self.client
+            .put(path.into())
+            .header("User-Agent", self.user_agent.clone())
+            .body(payload.into())
+            .await
     }
 
     pub async fn patch(
@@ -38,7 +58,11 @@ impl Agent {
         path: impl Into<String>,
         payload: impl Into<surf::Body>,
     ) -> Result<surf::Response, surf::Error> {
-        self.client.patch(path.into()).body(payload.into()).await
+        self.client
+            .patch(path.into())
+            .header("User-Agent", self.user_agent.clone())
+            .body(payload.into())
+            .await
     }
 
     pub async fn delete(
@@ -46,7 +70,11 @@ impl Agent {
         path: impl Into<String>,
         payload: impl Into<surf::Body>,
     ) -> Result<surf::Response, surf::Error> {
-        self.client.delete(path.into()).body(payload.into()).await
+        self.client
+            .delete(path.into())
+            .header("User-Agent", self.user_agent.clone())
+            .body(payload.into())
+            .await
     }
 }
 
@@ -59,18 +87,21 @@ mod tests {
     #[async_std::test]
     async fn test_agent_get() -> surf::Result<()> {
         let base_url = &mockito::server_url();
+        let user_agent = "BENCH_RS";
         let path = "/hello";
         let header_name = "content-type";
         let header_value = "text/plain";
         let body = "hello, world!!";
 
         let _m = mockito::mock("GET", path)
+            .match_header("User-Agent", user_agent)
             .with_status(surf::StatusCode::Ok as usize)
             .with_header(header_name, header_value)
             .with_body(body)
             .create();
 
-        let agent = Agent::new(base_url);
+        let mut agent = Agent::new(base_url);
+        agent.set_user_agent(user_agent);
         let mut response = agent.get(path).await?;
         assert_eq!(response.status(), surf::StatusCode::Ok);
         assert_eq!(response.header(header_name).unwrap(), header_value);
@@ -82,6 +113,7 @@ mod tests {
     #[async_std::test]
     async fn test_agent_post() -> surf::Result<()> {
         let base_url = &mockito::server_url();
+        let user_agent = "BENCH_RS";
         let path = "/hello";
         let payload = json!({ "hello": "world"});
         let header_name = "content-type";
@@ -89,12 +121,14 @@ mod tests {
         let body = r#"{"hello": "world"}"#;
 
         let _m = mockito::mock("POST", path)
+            .match_header("User-Agent", user_agent)
             .with_status(surf::StatusCode::Created as usize)
             .with_header(header_name, header_value)
             .with_body(body)
             .create();
 
-        let agent = Agent::new(base_url);
+        let mut agent = Agent::new(base_url);
+        agent.set_user_agent(user_agent);
         let mut response = agent.post(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Created);
         assert_eq!(response.header(header_name).unwrap(), header_value);
@@ -106,6 +140,7 @@ mod tests {
     #[async_std::test]
     async fn test_agent_put() -> surf::Result<()> {
         let base_url = &mockito::server_url();
+        let user_agent = "BENCH_RS";
         let path = "/hello";
         let payload = json!({ "hello": "world"});
         let header_name = "content-type";
@@ -113,12 +148,14 @@ mod tests {
         let body = r#"{"hello": "world"}"#;
 
         let _m = mockito::mock("PUT", path)
+            .match_header("User-Agent", user_agent)
             .with_status(surf::StatusCode::Created as usize)
             .with_header(header_name, header_value)
             .with_body(body)
             .create();
 
-        let agent = Agent::new(base_url);
+        let mut agent = Agent::new(base_url);
+        agent.set_user_agent(user_agent);
         let mut response = agent.put(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Created);
         assert_eq!(response.header(header_name).unwrap(), header_value);
@@ -130,6 +167,7 @@ mod tests {
     #[async_std::test]
     async fn test_agent_patch() -> surf::Result<()> {
         let base_url = &mockito::server_url();
+        let user_agent = "BENCH_RS";
         let path = "/hello";
         let payload = json!({ "hello": "world"});
         let header_name = "content-type";
@@ -137,12 +175,14 @@ mod tests {
         let body = r#"{"hello": "world"}"#;
 
         let _m = mockito::mock("PATCH", path)
+            .match_header("User-Agent", user_agent)
             .with_status(surf::StatusCode::Created as usize)
             .with_header(header_name, header_value)
             .with_body(body)
             .create();
 
-        let agent = Agent::new(base_url);
+        let mut agent = Agent::new(base_url);
+        agent.set_user_agent(user_agent);
         let mut response = agent.patch(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Created);
         assert_eq!(response.header(header_name).unwrap(), header_value);
@@ -154,6 +194,7 @@ mod tests {
     #[async_std::test]
     async fn test_agent_delete() -> surf::Result<()> {
         let base_url = &mockito::server_url();
+        let user_agent = "BENCH_RS";
         let path = "/hello";
         let payload = json!({ "hello": "world"});
         let header_name = "content-type";
@@ -161,12 +202,14 @@ mod tests {
         let body = r#"{"hello": "world"}"#;
 
         let _m = mockito::mock("DELETE", path)
+            .match_header("User-Agent", user_agent)
             .with_status(surf::StatusCode::Ok as usize)
             .with_header(header_name, header_value)
             .with_body(body)
             .create();
 
-        let agent = Agent::new(base_url);
+        let mut agent = Agent::new(base_url);
+        agent.set_user_agent(user_agent);
         let mut response = agent.delete(path, payload).await?;
         assert_eq!(response.status(), surf::StatusCode::Ok);
         assert_eq!(response.header(header_name).unwrap(), header_value);
