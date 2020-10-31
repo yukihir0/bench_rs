2a7aa35d5617f96e96379f166c82072210ade069
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-31 17:17:00 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 extern crate bench_rs;
 
+#[macro_use]
+extern crate clap;
+
 use anyhow::Result;
 use async_std;
 use bench_rs::agent::*;
 use bench_rs::benchmark::*;
 use bench_rs::errors::*;
 use bench_rs::score::*;
+use clap::{App, Arg};
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    let agent = Agent::new("https://example.com");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::new("base_url")
+                .about("benchmark target base url")
+                .short('b')
+                .long("base_url")
+                .value_name("BASE_URL")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let base_url = matches.value_of("base_url").unwrap();
+
+    let agent = Agent::new(base_url);
     let score = Score::new();
     let errors = Errors::new();
     let benchmark = Benchmark::new(agent, score, errors);
